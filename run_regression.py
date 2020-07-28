faa9c027250de36ace71855c78deb0c00787dde0
Benjamin Cai <benjamincaiyh@gmail.com>
2020-07-28 14:01:07 +0800

    TFMODEL TRAINER

--- run_regression.py
+++ run_regression.py
@@ -40,7 +40,7 @@ def compute_metrics(pred):
         'r2': r2_score(y_true, y_pred),
     }
 
-def run_bert_regression():
+def run_bert_regression_trainer():
     """ Run bert single class classification(a.k.a regression) model."""
     print('START TRAINNING FOR REGRESSION')
 
@@ -104,5 +104,65 @@ def run_bert_regression():
     with open(os.path.join(os.getenv('OUTPUT_DIR'), 'eval_results.json'), 'w') as fwrite:
         json.dump(result, fwrite, indent=4)
 
+def run_bert_regression_tfmodel():
+    """ Run BERT for regression as a tfmodel."""
+    print('START TRAINNING FOR REGRESSION')
+
+    tokenizer = AutoTokenizer.from_pretrained(os.getenv('MODEL_NAME'))
+    config = AutoConfig.from_pretrained(os.getenv('MODEL_NAME'), num_labels=1)
+    model = TFDistilBertForSequenceClassification.from_pretrained(os.getenv('MODEL_NAME'), config=config)
+
+    print('\nModel Config:')
+    print(config)
+    print('Tokenizer: ', tokenizer)
+    print('Model: ', model)
+
+    tc = TopCoder()
+    encoded_text = tc.get_bert_encoded_txt_features(tokenizer)
+    target = tc.get_target()
+
+    print(f'\nSize of dataset: {len(target)}')
+
+    dataset = tf.data.Dataset.from_tensor_slices((encoded_text, target))
+    dataset = dataset.shuffle(len(target))
+    train_ds, test_ds = dataset.take(int((4 / 5) * len(target))), dataset.skip(int((4 / 5) * len(target)))
+    train_ds = train_ds.batch(16)
+    test_ds = test_ds.batch(8)
+
+    print('\nTrain dataset samples:')
+    for el in train_ds.take(3):
+        pprint(el)
+    print('\nTest dataset samples:')
+    for el in test_ds.take(3):
+        pprint(el)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.RMSprop(learning_rate=2e-5),
+        loss='mse',
+        metrics=[
+            'mae',
+            'mse',
+            # tf.keras.metrics.MeanRelativeError(normalizer=[1])
+        ]
+    )
+    history = model.fit(
+        train_ds,
+        verbose=2,
+        epochs=3
+    )
+    result = model.evaluate(
+        test_ds,
+        verbose=2,
+        return_dict=True
+    )
+
+    pprint(result)
+
+    history_df = pd.DataFrame(history.history)
+    history_df.to_json(os.path.join(os.getenv('OUTPUT_DIR'), 'train_history.json'), orient='index', indent=4)
+    with open(os.path.join(os.getenv('OUTPUT_DIR'), 'result.json'), 'w') as f:
+        json.dump(result, f, indent=4)
+
+
 if __name__ == "__main__":
-    run_bert_regression()
+    run_bert_regression_tfmodel()
